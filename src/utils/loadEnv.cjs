8bafe90f697fa7186b371e476be9209fd5688425
BwinvitBoonsri <bwinvit@sobtid.me>
2024-03-14 17:55:53 +0700

    init

--- src/utils/loadEnv.cjs
+++
@@ -1,8 +0,0 @@
-const { resolve } = require("path");
-
-require("dotenv").config({
-    path:
-        process.env.NODE_ENV.trim() === "production"
-            ? resolve(__dirname, "../../config/.env.production")
-            : resolve(__dirname, "../../config/.env.development"),
-});
