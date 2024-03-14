404bfb8a0673b4c449044212959e6b2a5eca5004
BwinvitBoonsri <bwinvit@sobtid.me>
2024-03-14 18:07:49 +0700

    start project

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from "express";
+
+export const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+
+app.get("/test", (req, res) => {
+    res.send("APIs tested");
+});
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
