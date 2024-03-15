fdb5c3cd280f1034241ff4d0a56537039229c50e
BwinvitBoonsri <138432065+BwinvitBoonsri@users.noreply.github.com>
2024-03-15 09:47:36 +0700

    Fix #2

--- src/index.js
+++ src/index.js
@@ -4,15 +4,13 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.send("Hello World");
+  res.send("Hello World");
 });
 
 app.get("/test", (req, res) => {
-    res.send("APIs tested");
+  res.send("APIs tested");
 });
 
 app.listen(port, () => {
-    console.log(`Server started on port ${port}`);
+  console.log(`Server started on port ${port}`);
 });
-
-// export default app
